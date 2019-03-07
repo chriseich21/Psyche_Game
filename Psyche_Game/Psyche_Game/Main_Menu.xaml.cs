cf53f9edfb201c874f9d48593b45e318693141fd
SethATurnage <turnageseth@gmail.com>
2019-03-07 16:03:26 -0700

    added background
    added bg

---
+++ Psyche_Game/Psyche_Game/Main_Menu.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Psyche_Game
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Main_Menu : ContentView
+	{
+		public Main_Menu ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
